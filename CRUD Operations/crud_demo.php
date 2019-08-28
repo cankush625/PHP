9792776f54b3fa069a552bbc84511daf63b8c87f
cankush625 <cankush625@gmail.com>
2019-08-28 23:39:05 +0530

    Delete and search functions are added

--- CRUD Operations/crud_demo.php
+++ CRUD Operations/crud_demo.php
@@ -9,7 +9,7 @@
 	<body>
 		<h1>Enter user details</h1>
 		<form method="post" action="#">
-			Enter id : <input type="number" name="id" requuired>
+			Enter id : <input type="number" name="id" required>
 			Enter name : <input type="text" name="name" required>
 			Enter age : <input type="number" name="age" required><br>
 			<input type="submit" name="submit" value="Submit">&nbsp&nbsp
@@ -25,7 +25,10 @@
 			Enter name : <input type="text" name="uname" required>
 			<input type="submit" name="upname" value="Update">
 		</form>
+		<span>Enter Username to search : <input type="text" name="sname" required></span>
+		<input type="submit" name="search" value="Search"><br>
 
+		<!-- Inserting records in the database -->
 		<?php
 			if(isset($_POST["submit"])){
 				$que = "insert into user_info(id, name, age) values('$_POST[id]', '$_POST[name]', '$_POST[age]')";
@@ -33,6 +36,7 @@
 			}
 		?>
 
+		<!-- Deleting records from the database -->
 		<?php
 			if(isset($_POST["delete"])){
 				$que = "delete from user_info where id = '$_POST[did]'";
@@ -40,10 +44,43 @@
 			}
 		?>
 
+		<!-- Updating records in the database -->
 		<?php
 			if(isset($_POST["upname"])){
 				$que = "update user_info set name = '$_POST[uname]' where id = '$_POST[uid]'";
 				mysqli_query($conn, $que);
+			}
+		?>
+
+		<!-- Displaying records from the database -->
+		<?php
+			$res = mysqli_query($conn, "select * from user_info");
+			while($row = mysqli_fetch_array($res)) {
+				if ($row["id"] == 2) {
+					echo "You are logged in as ".$row["name"];
+				}
+			}
+		?>
+
+		<!-- Searching records in the database -->
+		<?php
+			if (isset($_POST["search"])) {
+				$res = mysqli_query($conn, "select * from user_info where name = '$_POST[sname]'");
+				echo "<table border=1>";
+					echo "<tr>";
+					echo "<th>"; echo "ID"; echo "</th>";
+					echo "<th>"; echo "Name"; echo "</th>";
+					echo "<th>"; echo "Age"; echo "</th>";
+					echo "</tr>";
+						
+				while($row = mysqli_fetch_array($res)) {
+					echo "<tr>";
+					echo "<td>"; echo $row["id"]; echo "</td>";
+					echo "<td>"; echo $row["name"]; echo "</td>";
+					echo "<td>"; echo $row["age"]; echo "</td>";
+					echo "</tr>";
+				}
+				echo "</table>";
 			}
 		?>
 	</body>
